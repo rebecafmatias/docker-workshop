32fd97d60593bac10e1118cd13f92af4159f9926
rebecafmatias <rebecafeitosamatias@gmail.com>
2025-12-12 23:53:36 -0300

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,8 +4,11 @@ import pandas as pd
 month = int(sys.argv[1])
 
 
-df = pd.DataFrame({"A":[1,2],"B":[3,4]})
+df = pd.DataFrame({"day":[1,2],"num_clients":[3,4]})
+df['month'] = month
 print(df)
+
+df.to_parquet(f"output_{month}.parquet")
 
 print("arguments", sys.argv)
 print(f"hello pipeline, month {month}")
