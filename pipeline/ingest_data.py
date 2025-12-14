e14877f33e23ee7de267cecf200150dd73a34f3b
rebecafmatias <rebecafeitosamatias@gmail.com>
2025-12-14 20:39:55 -0300

    updating ingestion file

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,70 +1,71 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-year = 2021
-month = 1
+def run():
+    year = 2021
+    month = 1
 
-pg_user = "root"
-pg_pass = "root"
-pg_host = "localhost"
-pg_db = "ny_taxi"
-pg_port = 5432
+    pg_user = "root"
+    pg_pass = "root"
+    pg_host = "localhost"
+    pg_db = "ny_taxi"
+    pg_port = 5432
 
-chunk_size = 10000
+    chunk_size = 10000
 
-prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/"
-url = f"{prefix}yellow_tripdata_{year}-{month}.csv.gz"
-
-
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
-
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
+    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/"
+    url = f"{prefix}yellow_tripdata_{year}-{month}.csv.gz"
 
 
-df = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates
-)
+    dtype = {
+        "VendorID": "Int64",
+        "passenger_count": "Int64",
+        "trip_distance": "float64",
+        "RatecodeID": "Int64",
+        "store_and_fwd_flag": "string",
+        "PULocationID": "Int64",
+        "DOLocationID": "Int64",
+        "payment_type": "Int64",
+        "fare_amount": "float64",
+        "extra": "float64",
+        "mta_tax": "float64",
+        "tip_amount": "float64",
+        "tolls_amount": "float64",
+        "improvement_surcharge": "float64",
+        "total_amount": "float64",
+        "congestion_surcharge": "float64"
+    }
 
+    parse_dates = [
+        "tpep_pickup_datetime",
+        "tpep_dropoff_datetime"
+    ]
+    
+    SQLALCHEMY_DB_URL = f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
 
-SQLALCHEMY_DB_URL = f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
+    engine = create_engine(SQLALCHEMY_DB_URL)
 
-engine = create_engine(SQLALCHEMY_DB_URL)
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=10000
+    )
 
-df.head(0).to_sql(name="yellow_taxi_data", con=engine, if_exists="replace")
+    pd.io.sql.get_schema(df_iter, name="yellow_taxi_data", con=engine)
 
-pd.io.sql.get_schema(df, name="yellow_taxi_data", con=engine)
+    first = True
+    for df_chunk in df_iter:
+        if first:
+            df_chunk.head(0).to_sql(
+                name="yellow_taxi_data", 
+                con=engine, 
+                if_exists="replace")
+            
+            first = False
 
-df_iter = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=10000
-)
-
-for df_chunk in df_iter:
-    df_chunk.to_sql(name="yellow_taxi_data",con=engine,if_exists="append")
-    print(len(df))
+        df_chunk.to_sql(name="yellow_taxi_data",
+                        con=engine,
+                        if_exists="append")
 
